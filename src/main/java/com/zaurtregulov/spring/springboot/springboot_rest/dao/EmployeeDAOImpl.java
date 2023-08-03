0345c4ca3e9bb4be9f8e5991e5b73a17eb15e95b
alexa <alexanderredar1@gmail.com>
2023-08-03 23:03:44 +0300

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/springboot/springboot_rest/dao/EmployeeDAOImpl.java
@@ -0,0 +1,56 @@
+package com.zaurtregulov.spring.springboot.springboot_rest.dao;
+
+import com.zaurtregulov.spring.springboot.springboot_rest.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Session session = entityManager.unwrap(Session.class);
+
+//        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class)
+//                .getResultList();
+
+        Query<Employee> query = session.createQuery("from Employee",Employee.class);
+        List<Employee> allEmployees = query.getResultList();
+
+        return allEmployees;
+    }
+
+//    @Override
+//    public void saveEmployee(Employee employee) {
+//        Session session = sessionFactory.getCurrentSession();
+//
+//        session.saveOrUpdate(employee);
+//    }
+//
+//    @Override
+//    public Employee getEmployee(int id) {
+//        Session session = sessionFactory.getCurrentSession();
+//
+//        Employee employee = session.get(Employee.class, id);
+//
+//        return employee;
+//    }
+//
+//    @Override
+//    public void deleteEmployee(int id) {
+//        Session session = sessionFactory.getCurrentSession();
+//        Query<Employee> query = session.createQuery("delete from Employee " +
+//                "where id = :employeeId");
+//        query.setParameter("employeeId", id);
+//        query.executeUpdate();
+//    }
+}
